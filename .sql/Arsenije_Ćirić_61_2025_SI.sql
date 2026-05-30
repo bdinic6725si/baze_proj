0179cbe5e069fdfc2fd3320782a850a7b4721df4
aciric6125si <aciric6125si@raf.rs>
2026-05-30 21:54:58 +0200

    Update 9

--- .sql/Arsenije_Ćirić_61_2025_SI.sql
+++ .sql/Arsenije_Ćirić_61_2025_SI.sql
@@ -13,11 +13,12 @@ SELECT
     s.Datum                AS Datum_Sesije,
     o.Napomena
 FROM Opservacija o
-JOIN Sesija s              ON s.ID_Sesije           	= o.ID_Sesije
-JOIN Laboratorija l        ON l.ID_Lab              	= s.ID_Lab
-JOIN Izvodjenje iz         ON iz.ID_Izvodjenja      	= s.ID_Izvodjenja
-JOIN Eksperiment e         ON e.ID_Eksperimenta     	= iz.ID_Eksperimenta
-JOIN Nebeski_Objekat no    ON no.ID_Nebeskog_Objekta 	= o.ID_Nebeskog_Objekta
+JOIN Sesija s              ON s.ID_Sesije            = o.ID_Sesije
+JOIN Laboratorija l        ON l.ID_Lab               = s.ID_Lab
+JOIN Izvodjenje iz         ON iz.ID_Izvodjenja       = s.ID_Izvodjenja
+JOIN Eksperiment e         ON e.ID_Eksperimenta      = iz.ID_Eksperimenta
+JOIN Op_O_Objektu ooo      ON ooo.ID_Opservacije     = o.ID_Opservacije
+JOIN Nebeski_Objekat no    ON no.ID_Nebeskog_Objekta = ooo.ID_Nebeskog_Objekta
 ORDER BY 
     FIELD(o.Kvalitet_Podataka, 'odlican', 'dobar', 'slab', 'neupotrebljiv'),
     o.SNR DESC;
